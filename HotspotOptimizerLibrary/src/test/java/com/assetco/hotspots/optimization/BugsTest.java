bcf2096a5d6e1d0a2eee9be735a86858883bf6bf
sgh2024 <155660939+sgh2024@users.noreply.github.com>
2024-01-04 20:47:24 +0100

    Describe the first “critical” bug in a testable format that will “grow up” into a test.

--- HotspotOptimizerLibrary/src/test/java/com/assetco/hotspots/optimization/BugsTest.java
+++ HotspotOptimizerLibrary/src/test/java/com/assetco/hotspots/optimization/BugsTest.java
@@ -1,11 +1,67 @@
 package com.assetco.hotspots.optimization;
 
+import com.assetco.search.results.Asset;
+import com.assetco.search.results.AssetVendor;
+import com.assetco.search.results.AssetVendorRelationshipLevel;
+import com.assetco.search.results.HotspotKey;
 import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 class BugsTest
 {
-  @Test
-  public void precedingPartnerWithLongTrailingAssetsDoesNotWin() {
 
+  private static final int NUM_PARTNER_CONSECUTIVE_ASSETS = 4;
+
+  @Test
+  void precedingPartnerWithLongTrailingAssetsDoesNotWin()
+  {
+    // ARRANGE
+    AssetVendor partnerVendor = makeVendor(AssetVendorRelationshipLevel.Partner);
+    Asset missing = givenAssetInResultsWithVendor(partnerVendor);
+    AssetVendor otherPartnerVendor = makeVendor(AssetVendorRelationshipLevel.Partner);
+    Asset otherDisrupting = givenAssetInResultsWithVendor(partnerVendor);
+    List<Asset> expected = makeConsecutiveAssets(partnerVendor);
+    // ACT
+    whenOptimize();
+    // ASSERT
+    thenHotspotDoesNotHave(HotspotKey.Showcase, missing);
+    thenHotspotHasExactly(HotspotKey.Showcase, expected);
+  }
+
+  private void thenHotspotHasExactly(HotspotKey hotspotKey, List<Asset> expected)
+  {
+
+  }
+
+  private void thenHotspotDoesNotHave(HotspotKey hotspotKey, Asset... missing)
+  {
+
+  }
+
+  private void whenOptimize()
+  {
+
+  }
+
+  private List<Asset> makeConsecutiveAssets(AssetVendor partnerVendor)
+  {
+    List<Asset> result = new ArrayList<>();
+
+    for (int i = 0; i < NUM_PARTNER_CONSECUTIVE_ASSETS; i++) {
+      result.add(givenAssetInResultsWithVendor(partnerVendor));
+    }
+    return result;
+  }
+
+  private Asset givenAssetInResultsWithVendor(AssetVendor partnerVendor)
+  {
+    return null;
+  }
+
+  private AssetVendor makeVendor(AssetVendorRelationshipLevel assetVendorRelationshipLevel)
+  {
+    return null;
   }
 }
