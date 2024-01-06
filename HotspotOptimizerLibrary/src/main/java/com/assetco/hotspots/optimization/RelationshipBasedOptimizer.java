a0afd567799c5ef48a3f56db0a16aeaabb97a9af
sgh2024 <155660939+sgh2024@users.noreply.github.com>
2024-01-06 18:10:32 +0100

    Fix First Bug

--- HotspotOptimizerLibrary/src/main/java/com/assetco/hotspots/optimization/RelationshipBasedOptimizer.java
+++ HotspotOptimizerLibrary/src/main/java/com/assetco/hotspots/optimization/RelationshipBasedOptimizer.java
@@ -25,8 +25,9 @@ class RelationshipBasedOptimizer {
     public void optimize(SearchResults searchResults) {
         Iterator<Asset> iterator = searchResults.getFound().iterator();
         // don't affect a showcase built by an earlier rule
-        var showcaseFull = searchResults.getHotspot(Showcase).getMembers().size() > 0;
-        var showcaseAssets = new ArrayList<Asset>();
+        var showcaseFull = !searchResults.getHotspot(Showcase).getMembers().isEmpty();
+        var showcaseCandidateAssets = new HashMap<AssetVendor, ArrayList<Asset>>();
+        List<Asset> showcaseAssets = null;
         var partnerAssets = new ArrayList<Asset>();
         var goldAssets = new ArrayList<Asset>();
         var silverAssets = new ArrayList<Asset>();
@@ -45,50 +46,26 @@ class RelationshipBasedOptimizer {
             // remember this partner asset
             partnerAssets.add(asset);
 
-            // too many assets in showcase - put in top picks instead...
-            if (showcaseAssets.size() >= 5) {
-                if (Objects.equals(showcaseAssets.get(0).getVendor(), asset.getVendor()))
-                    searchResults.getHotspot(TopPicks).addMember(asset);
+            if (showcaseAssets != null) {
+                if (Objects.equals(showcaseAssets.get(0).getVendor(), asset.getVendor())) {
+                    // too many assets in showcase - put in top picks instead...
+                    if (showcaseAssets.size() >= 5)
+                        searchResults.getHotspot(TopPicks).addMember(asset);
+                    else
+                        showcaseAssets.add(asset);
+                }
             } else {
-                // TODO:
-                // ***** MUST NOT FORGET! SHOWCASING BUG! CONTRACTUAL! *****
-                // I asked Jamie about rule #2 of the requirements and she didn't get back to me. I made by best guess.
-                // Just before release, we find out that my guess was wrong. It's the first partner TO REACH the 3-asset
-                // minimum for a set of search results.
-                // 
-                // Right now, if a sequence of partner assets is broken by an asset for another partner, we stop counting
-                // the first partner's assets. So it's possible for a partner to lose their showcase or for another, lower
-                // priority result to "steal" the showcase from them.
-                //
-                // We need to be keeping track of a map of lists - one for each partner - and "lock in" the first partner
-                // that gets three assets in their list.
-                //
-                // This is OBVIOUSLY very bad but we can't do anything about it, right now. We have to release the product
-                // as is. This needs to be one of the very first things we address after we release.
-                //
-                // I'm leaving this huge comment here to ensure we don't forget it because...
-                //
-                // ...WE ABSOLUTELY MUST NOT LET THIS SLIP THROUGH THE CRACKS!!!
-                //
-                // -jownw
-                // 2/9/07
-                // ***** MUST NOT FORGET! SHOWCASING BUG! CONTRACTUAL! *****
-
-                // if there are already assets from a different vendor but not enough to hold showcase,
-                // clear showcase
-                if (showcaseAssets.size() != 0)
-                    if (!Objects.equals(showcaseAssets.get(0).getVendor(), asset.getVendor()))
-                        if (showcaseAssets.size() < 3)
-                            showcaseAssets.clear();
-
                 // add this asset to an empty showcase or showcase with same vendor in it
-                // if there's already another vendor, that vendor should take precedence!
-                if (showcaseAssets.size() == 0 || Objects.equals(showcaseAssets.get(0).getVendor(), asset.getVendor()))
-                    showcaseAssets.add(asset);
+                var currentAssets = getAssets(showcaseCandidateAssets, asset.getVendor());
+                currentAssets.add(asset);
+                // the first partner TO REACH the 3-asset minimum for a set of search
+                // results owns the showcase.
+                if (currentAssets.size() >= 3)
+                    showcaseAssets = currentAssets;
             }
         }
 
-        // [DBV], 4/14/2014: 
+        // [DBV], 4/14/2014:
         // need added this here even though it's not about this rules
         // frm Jamie,
         // 1. All partner assets should be eligible for high-value slots in the main grid.
@@ -105,7 +82,7 @@ class RelationshipBasedOptimizer {
             searchResults.getHotspot(Fold).addMember(asset);
 
         // only copy showcase assets into hotspot if there are enough for a partner to claim the showcase
-        if (!showcaseFull && showcaseAssets.size() >= 3) {
+        if (!showcaseFull && showcaseAssets != null) {
             Hotspot showcaseHotspot = searchResults.getHotspot(Showcase);
             for (Asset asset : showcaseAssets)
                 showcaseHotspot.addMember(asset);
@@ -123,5 +100,15 @@ class RelationshipBasedOptimizer {
         // LOL acw-14511: gold assets should appear in fold box when appropriate
         for (var asset : silverAssets)
             searchResults.getHotspot(Fold).addMember(asset);
+    }
+
+    private static ArrayList<Asset> getAssets(HashMap<AssetVendor, ArrayList<Asset>> map,
+                                              AssetVendor vendor) {
+        var assets = map.get(vendor);
+        if (assets == null) {
+            map.put(vendor, new ArrayList<>());
+            return map.get(vendor);
+        }
+        return assets;
     }
 }
