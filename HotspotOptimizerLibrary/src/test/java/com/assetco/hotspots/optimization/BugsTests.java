abea91339946cb75c9e51ad35aff6fb4aaeefb42
sgh2024 <155660939+sgh2024@users.noreply.github.com>
2024-01-07 15:02:39 +0100

    Second BugFix - Test: test buggy behaviour

--- HotspotOptimizerLibrary/src/test/java/com/assetco/hotspots/optimization/BugsTests.java
+++ HotspotOptimizerLibrary/src/test/java/com/assetco/hotspots/optimization/BugsTests.java
@@ -9,6 +9,8 @@ import java.util.*;
 
 import static com.assetco.search.results.AssetVendorRelationshipLevel.*;
 import static com.assetco.search.results.HotspotKey.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BugsTests {
     private static final int NUM_PARTNER_CONSECUTIVE_ASSETS = 4;
@@ -22,6 +24,15 @@ class BugsTests {
         AssetPurchaseInfo info = new AssetPurchaseInfo(1, 1, money, money);
         List<AssetTopic> topics = new ArrayList<>();
         return new Asset(string, string, uri, uri, info, info, topics, vendor);
+    }
+
+    private static Asset makeAssetWithTopics(AssetVendor vendor,
+                                             AssetTopic... topics) {
+        String string = "any";
+        URI uri = URI.create(string);
+        Money money = new Money(BigDecimal.ZERO);
+        AssetPurchaseInfo info = new AssetPurchaseInfo(1, 1, money, money);
+        return new Asset(string, string, uri, uri, info, info, Arrays.asList(topics), vendor);
     }
 
     private static AssetVendor makeVendor(AssetVendorRelationshipLevel relationshipLevel) {
@@ -50,6 +61,23 @@ class BugsTests {
         thenHotspotHasExactly(Showcase, expected);
     }
 
+    @Test
+    void notAllLowPriorityHotTopicsAreHighlighted() {
+        // ARRANGE
+        var highPriorityTopic = new AssetTopic("0", "0");
+        var lowPriorityTopic = new AssetTopic("1", "1");
+        var vendor = makeVendor(Basic);
+        var expected = new ArrayList<>(makeConsecutiveAssetsWithTopics(1, vendor, lowPriorityTopic));
+        expected.addAll(makeConsecutiveAssetsWithTopics(3, vendor, highPriorityTopic));
+        var missing = makeConsecutiveAssetsWithTopics(1, vendor, lowPriorityTopic);
+        setHotTopics(highPriorityTopic, lowPriorityTopic);
+        // ACT
+        whenOptimize();
+        // ASSERT
+        thenHotspotContains(Highlight, expected);
+        thenHotspotDoesNotContain(Highlight, missing);
+    }
+
     private void thenHotspotHasExactly(HotspotKey hotspotKey,
                                        List<Asset> expected) {
         var hotspotMembers = searchResults.getHotspot(hotspotKey).getMembers().toArray();
@@ -57,8 +85,24 @@ class BugsTests {
         Assertions.assertArrayEquals(expectedMembers, hotspotMembers);
     }
 
+    private void thenHotspotContains(HotspotKey hotspotKey,
+                                     List<Asset> expected) {
+        var hotspotMembers = searchResults.getHotspot(hotspotKey).getMembers();
+        assertTrue(hotspotMembers.containsAll(expected));
+    }
+
+    private void thenHotspotDoesNotContain(HotspotKey hotspotKey,
+                                           List<Asset> expected) {
+        var hotspotMembers = searchResults.getHotspot(hotspotKey).getMembers();
+        assertFalse(expected.stream().anyMatch(hotspotMembers::contains));
+    }
+
     private void whenOptimize() {
         optimizer.optimize(searchResults);
+    }
+
+    private void setHotTopics(AssetTopic... topics) {
+        optimizer.setHotTopics(() -> Arrays.asList(topics));
     }
 
     private List<Asset> makeConsecutiveAssetsWithVendor(AssetVendor vendor) {
@@ -70,8 +114,26 @@ class BugsTests {
         return result;
     }
 
+    private List<Asset> makeConsecutiveAssetsWithTopics(int num,
+                                                        AssetVendor vendor,
+                                                        AssetTopic... topics) {
+        List<Asset> result = new ArrayList<>();
+
+        for (int i = 0; i < num; i++) {
+            result.add(givenAssetInResultsWithTopics(vendor, topics));
+        }
+        return result;
+    }
+
     private Asset givenAssetInResultsWithVendor(AssetVendor vendor) {
         Asset asset = makeAssetWithVendor(vendor);
+        searchResults.addFound(asset);
+        return asset;
+    }
+
+    private Asset givenAssetInResultsWithTopics(AssetVendor vendor,
+                                                AssetTopic... topics) {
+        Asset asset = makeAssetWithTopics(vendor, topics);
         searchResults.addFound(asset);
         return asset;
     }
